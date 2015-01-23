70be1e8df545aaa1bb8b6b578ad63be1b71e6f23
MacKenzie <mackenzieajudd@gmail.com>
2015-01-22 16:44:17 -0800

    Re-added plc variable reference in state refresh page.

--- WebMockup/Pneuman/Scripts/MainScript.js
+++ WebMockup/Pneuman/Scripts/MainScript.js
@@ -1,13 +1,9 @@
 function hide_element(element_id){
-    //document.getElementById(element_id).style.visibility = 'hidden';
-    //document.getElementById(element_id).style.opacity = '0';
     element_id = id_to_selector(element_id);
     $(element_id).fadeOut(1000);
 }
 
 function show_element(element_id){
-    //document.getElementById(element_id).style.visibility = 'visible';
-    //document.getElementById(element_id).style.opacity = '100';
     element_id = id_to_selector(element_id);
     $(element_id).fadeIn( 1000);
 }
