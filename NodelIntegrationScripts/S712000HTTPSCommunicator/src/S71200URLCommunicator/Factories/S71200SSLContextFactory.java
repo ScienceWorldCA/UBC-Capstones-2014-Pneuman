c69f53848bd6d417539f17bfc4d682a682af5755
MacKenzie <mackenzieajudd@gmail.com>
2015-03-07 11:48:09 -0800

    Removed old project folder.

--- NodelIntegrationScripts/S712000HTTPSCommunicator/src/S71200URLCommunicator/Factories/S71200SSLContextFactory.java
+++
@@ -1,7 +0,0 @@
-package S71200URLCommunicator.Factories;
-
-/**
- * Created by macke_000 on 3/6/2015.
- */
-public class S71200SSLContextFactory {
-}
--- NodelIntegrationScripts/S712000HTTPSCommunicator/src/S71200URLCommunicator/Interfaces/SSLContextFactory.java
+++
@@ -1,7 +0,0 @@
-package S71200URLCommunicator.Interfaces;
-
-/**
- * Created by macke_000 on 3/6/2015.
- */
-public class SSLContextFactory {
-}
